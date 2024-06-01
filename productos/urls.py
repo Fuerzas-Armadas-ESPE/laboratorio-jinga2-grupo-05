1af62242e37d729b253c5e6c669b28c55cf12b16
LeonardoYaranga <ljyaranga@espe.edu.ec>
2024-06-01 11:29:06 -0500

    edit + delete

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     # URLs de vistas normales
     # Crear la URL de la vista index
-    path('',views.listar_productos, name='listar_productos')
+    path('',views.listar_productos, name='listar_productos'),
+    path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
+    path('borrar/<int:id>/', views.eliminar_producto, name='borrar_producto'),
+
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404, redirect
 from .models import Producto
 
 productos = []
@@ -8,3 +8,19 @@ def listar_productos(request):
     # Renderiza la plantilla listar.html
     productos = Producto.objects.all()
     return render(request,'listar.html',{'productos':productos})
+
+def editar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    if request.method == "POST":
+        producto.nombre = request.POST.get('nombre', '')
+        producto.precio = float(request.POST.get('precio', 0))
+        producto.cantidad = int(request.POST.get('cantidad', 0))
+        producto.save()
+        return redirect('listar_productos')
+    return render(request, 'editar.html', {'producto': producto})
+
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    return redirect('listar_productos')
