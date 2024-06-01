7222ed892ff49d3c582176636a388a586eab2718
LeonardoYaranga <ljyaranga@espe.edu.ec>
2024-05-31 19:31:58 -0500

    add productos

--- productos/views.py
+++ productos/views.py
@@ -6,3 +6,5 @@ productos = []
 def listar_productos(request):
     # Consulta a la base de datos
     # Renderiza la plantilla listar.html
+    productos = Producto.objects.all()
+    return render(request,'listar.html',{'productos':productos})
